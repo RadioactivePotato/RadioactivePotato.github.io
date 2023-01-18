d97b60b4d1736349efb92238ba73a9d0b91650e0
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2023-01-18 19:37:41 +0000

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "Not a bug, a feature.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "Google",
+    title: "10 Things To Know About Azure Static Web Apps 🎉",
     description:
-      "placeholder",
-    url: "https://google.com",
+      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Google",
+    title: "Web Development for Beginners",
     description:
-      "placeholder",
-    url: "https://google.com",
+      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "Google",
+    title: "My Resume Site",
     description:
-      "placeholder",
-    url: "https://google.com",
+      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "Google",
+    title: "GitHub Codespaces and github.dev",
     description:
-      "placeholder",
-    url: "https://google.com",
+      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
+    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
 
